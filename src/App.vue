0bb146068cff80bbba701ffbd48cee61bb1c1b4f
Dawid M <dawmic19@gmail.com>
2021-09-21 21:00:55 +0200

    desktop screen

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,60 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div class="desktop-container">
+      <div class="desktop-paragraphs-container">
+        <img class="logo-image" src="@/assets/logo.png" alt="logo" />
+        <p class="desktop-paragraph">Please open the app from mobile device.</p>
+      </div>
+      <img class="mockup-image" src="@/assets/Mockup.png" alt="mockup" />
     </div>
-    <router-view/>
   </div>
 </template>
 
 <style lang="scss">
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+html {
+  font-size: 62.5%;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  background-color: #1d1c28;
+
+  .desktop-container {
+    height: 100vh;
+    width: 100%;
+    margin: 0 auto;
+    display: none;
+
+    @media (min-width: 769px) {
+      display: flex;
+      flex-direction: row;
+      justify-content: center;
+      align-items: center;
+    }
+
+    .desktop-paragraphs-container {
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      align-items: center;
+
+      .desktop-paragraph {
+        font-size: 2.8rem;
+        color: #d6d5da;
+        margin-top: 1rem;
+        line-height: 1.5;
+      }
+    }
+  }
 }
 
 #nav {
