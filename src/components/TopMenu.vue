450d433ecd3be77191b648b03065b17bc0f8ca5f
Dawid M <dawmic19@gmail.com>
2021-09-26 16:50:47 +0200

    create the top menu tabs and set up routing

--- src/components/TopMenu.vue
+++ src/components/TopMenu.vue
@@ -13,6 +13,11 @@
         market_change_24hr
       }}</span>
     </div>
+    <div class="tabs-container">
+      <router-link to="/" tag="button"> All Currencies </router-link>
+      <router-link to="/TrackedCurrencies" tag="button">
+        TrackedCurrencies </router-link>
+      </div>
   </div>
 </template>
 
@@ -31,20 +36,20 @@ export default {
   width: 100%;
   display: flex;
   flex-direction: column;
-  justify-content: center;
+  justify-content: space-between;
   align-items: center;
   .top-logo {
     height: 1.9rem;
-    margin-bottom: 2rem;
+    margin-top: 4rem;
   }
   .top-market-paragraph {
     font-size: 2.2rem;
     color: #d6d5da;
     font-family: "Roboto", sans-serif;
-    flex: 0 1 10%;
   }
   .top-market-green-arrow,
   .top-market-red-arrow {
+    margin-top: -3rem;
     display: flex;
     flex-direction: row;
     justify-content: space-around;
@@ -53,7 +58,7 @@ export default {
     img {
       height: 1.8rem;
       width: 3.1rem;
-      margin: .2rem;
+      margin: 0.2rem;
     }
     span {
       font-family: "Roboto" sans-serif;
@@ -65,6 +70,26 @@ export default {
   }
   .top-market-red-arrow span {
     color: #db4848;
+  }
+  .tabs-container {
+    align-self: stretch;
+    display: flex;
+    flex-direction: row;
+    button {
+      width: 100%;
+      height: 4rem;
+      font-family: "Roboto" sans-serif;
+      font-size: 1.6rem;
+      color: #898989;
+      background: transparent;
+      outline: none;
+      border: none;
+      border-bottom: 1px solid #898989;
+      &.router-link-exact-active {
+        border-bottom: 1px solid #d6d5da;
+        color: #d6d5da;
+      }
+    }
   }
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,35 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import AllCurrencies from '@/components/AllCurrencies.vue'
+import TrackedCurrencies from '@/components/TrackedCurrencies.vue'
 
 Vue.use(VueRouter)
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+const routes = [{
+        path: '/about',
+        name: 'About',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () =>
+            import ( /* webpackChunkName: "about" */ '../views/About.vue')
+    },
+    {
+        path: '/',
+        name: 'AllCurrencies',
+        component: AllCurrencies
+    },
+    {
+        path: '/TrackedCurrencies',
+        name: 'TrackedCurrencies',
+        component: TrackedCurrencies
+    }
 ]
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes
 })
 
 export default router
